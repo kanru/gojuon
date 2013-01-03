1db93c0345fad8db78f81fe12b79f48b0d53deb8
Kan-Ru Chen (陳侃如) <kanru@kanru.info>
2013-01-03 09:52:54 +0800

    Add answer

--- js/gojuon.js
+++ js/gojuon.js
@@ -1,4 +1,4 @@
-var table = [
+var deck = [
     {hiragana: "あ", roma: "a"},
     {hiragana: "い", roma: "i"},
     {hiragana: "う", roma: "u"},
@@ -6,13 +6,28 @@ var table = [
     {hiragana: "お", roma: "o"},
 ];
 
+function setupBinding() {
+    var romaElm = document.getElementById("roma");
+    romaElm.addEventListener("click", function onClick(e) {
+        this.classList.toggle("hidden");
+    });
+}
+
 function pick() {
-    return Math.floor(Math.random()*table.length);
+    return Math.floor(Math.random()*deck.length);
+}
+
+function refresh() {
+    var hiraganaElm = document.getElementById("hiragana");
+    var romaElm = document.getElementById("roma");
+    var card = deck[pick()];
+    hiraganaElm.innerHTML = card.hiragana;
+    romaElm.innerHTML = '/'+card.roma+'/';
 }
 
 function init() {
-    var testElm = document.getElementById("test");
-    testElm.innerHTML = table[pick()].hiragana;
+    setupBinding();
+    refresh();
 }
 
 init();
